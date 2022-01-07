0a59c0545f9b930fa5378efdd1d0e255fcd51f4e
PeterT <tranpp87@gmail.com>
2022-01-07 14:23:08 -0500

    get search functionality to work

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Axios from 'axios';
 import './App.css'
+import PokemonList from './PokemonList';
 
 const App = () => {
+
+  const [pokemonName, setPokemonName] = useState('')
+
+  const searchPokemon = () => {
+    Axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`).then((response)=> console.log(response))
+  }
+
   return (
     <div className="App">
-      <h1>Pokedex</h1>
+      <div className='header-block'>
+        <h1>Pokédex</h1>
+        <div className='inputs'>
+        <input 
+          type='text' 
+          placeholder='ex. Pikachu'
+          onChange={(event) => {
+            setPokemonName(event.target.value)
+          }}
+          
+          />
+        <button 
+        type='submit'
+        onClick={searchPokemon}
+        >Search</button>
+        </div>
+      </div>
     </div>
   );
 }
